dfcbd7574f79a6ce86f412a68f930c531c8af104
BakungaBronson <bakungabro@gmail.com>
2020-11-19 08:22:57 +0300

    Removed What we do from index

--- index.php
+++ index.php
@@ -30,22 +30,6 @@
         </div>
     </section>
 
-    <section class="whatWeDo display" id="whatWeDo">
-        <h1>What We Do</h1>
-        <div class="row d-flex justify-content-center align-items-center mt-3">
-            <div class="col-8 justify-content-center align-items-center text-center">
-                <i class='bx bxs-tree bx-md'></i>
-                <p>
-                    We partner with different companies and stakeholders with a heart for environmental stewardship to implement key environmental projects in Uganda and Africa.
-                </p>
-                <i class='bx bxs-tree bx-md'></i>
-                <p>
-                    Our current projects include; Entasesa tree planting project, and Clean Uganda (a sanitation, and waste disposal project engaging children (schools) to take the lead role in protecting and preserving their environment).
-                </p>
-            </div>
-        </div>
-    </section>
-
     <section class="gallery display variation" id="gallery">
         <h1>Images</h1>
         <div id="carousel" class="carousel slide carousel-fade" data-ride="carousel" data-interval="6000">
